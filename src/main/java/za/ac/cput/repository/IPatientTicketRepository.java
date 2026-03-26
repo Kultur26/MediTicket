dcfe26c5cf006fe4fa6c31681528a81603224086
Joshua Adams <230317693@mycput.ac.za>
2026-03-26 10:04:29 +0200

    added classes for 230317693 part

---
+++ src/main/java/za/ac/cput/repository/IPatientTicketRepository.java
@@ -0,0 +1,13 @@
+/* IPatientTicketRepository.java
+   Repository interface for PatientTicket
+   Author: Joshua A (230317693)
+   Date: 22 March 2026
+*/
+
+package za.ac.cput.repository;
+
+import za.ac.cput.domain.PatientTicket;
+
+public interface IPatientTicketRepository extends IRepository<PatientTicket, Integer>{
+
+}
---
+++ src/main/java/za/ac/cput/repository/ITicketStatusRepository.java
@@ -0,0 +1,12 @@
+/* ITicketStatusRepository.java
+   Repository interface for TicketStatus
+   Author: Joshua A (230317693)
+   Date: 22 March 2026
+*/
+
+package za.ac.cput.repository;
+
+import za.ac.cput.domain.TicketStatus;
+
+public interface ITicketStatusRepository extends IRepository<TicketStatus, Integer>{
+}
